afd740e4fc02aa39c36cf97cfced7692bb5abeee
ydvdh <davdh7@outlook.com>
2017-05-24 21:54:58 -0500

    update

--- Example2DI/app.js
+++ Example2DI/app.js
@@ -5,7 +5,7 @@
 
   function DIFunction($scope,$filter,$injector) {
     $scope.Amount1=0;
-    $scope.Amount2=0;
+   
     $scope.getAmount1=function() {
       $scope.Amount1=addDoller($scope.Amount1);
 
@@ -15,14 +15,9 @@
       var result1=$filter("currency");
       return result1(input);
     }
-    $scope.getAmount2=function(){
-      $scope.Amount2=addPound($scope.Amount2);
-    }
+    
 
-    function addPound(input){
-      var result = $filter("currency");
-      return result(input);
-    }
+   
 
   }
 
